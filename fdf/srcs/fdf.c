1c66921fbe6683fd8590d0b8f8f6ae811a53c5ed
tsetsefly <tsetsefly@gmail.com>
2016-11-28 23:28:45 -0800

    adding in peer grading files

--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -27,21 +27,15 @@ void		draw_line(double x1, double y1, double x2, double y2, void *mlx, void *win
 	double	i;
 	double	slope;
 	double	y_int;
-	// double	x_int;
 	double	max;
 	double	res;
 	int		neg;
 
-	// mlx_pixel_put(mlx, window, x1, y1, color);
-	// mlx_pixel_put(mlx, window, x2, y2, color);
-
 	slope = (y2 - y1) / (x2 - x1);
 	y_int = y1 - slope * x1;
-	// x_int = (y_int * -1) / slope;
 	// printf("START: slope = %f,\ty-int = %f,\tx-int = %f,\tx2 - x1 = %f\n", slope, y_int, x_int, x2 - x1);
 	i = 0;
 	res = (0.5);
-	// max = (fabs(x2 - x1) > fabs(y2 - y1)) ? fabs(x2 - x1) : fabs(y2 - y1);
 	if (fabs(slope) < 1)
 	{
 		max = fabs(x2 - x1);
@@ -83,7 +77,7 @@ void		file_detective(char *file_name)
 	int		fd;
 	char	*line;
 
-	// if ((fd = open("file", O_RDONLY)) == -1) //
+	// if ((fd = open("file", O_RDONLY)) == -1)
 	// 	return (-1);
 	fd = open(file_name, O_RDONLY);
 	while (get_next_line(fd, &line) != 0)
@@ -94,90 +88,84 @@ void		file_detective(char *file_name)
 	close(fd);
 }
 
+void		test_print_spiral(void *mlx, void *window) // REMOVE LATER!!!
+{
+	int		color = 140 * 65536; // RGB = (R*65536)+(G*256)+B , (when R is RED, G is GREEN and B is BLUE)
+	int 	color_change = 8;
+
+	// VERTICAL LINE UP --> DOWN HERE
+	draw_line(400, 200, 400, 400, mlx, window, color += color_change);
+
+	draw_line(450, 200, 400, 400, mlx, window, color += color_change);
+	draw_line(500, 200, 400, 400, mlx, window, color += color_change);
+	draw_line(550, 200, 400, 400, mlx, window, color += color_change);
+	
+	draw_line(600, 200, 400, 400, mlx, window, color += color_change);
+	
+	draw_line(600, 250, 400, 400, mlx, window, color += color_change);
+	draw_line(600, 300, 400, 400, mlx, window, color += color_change);
+	draw_line(600, 350, 400, 400, mlx, window, color += color_change);
+	
+	draw_line(600, 400, 400, 400, mlx, window, color += color_change);
+	
+	draw_line(600, 450, 400, 400, mlx, window, color += color_change);
+	draw_line(600, 500, 400, 400, mlx, window, color += color_change);
+	draw_line(600, 550, 400, 400, mlx, window, color += color_change);
+	
+	draw_line(600, 600, 400, 400, mlx, window, color += color_change);
+
+	draw_line(550, 600, 400, 400, mlx, window, color += color_change);
+	draw_line(500, 600, 400, 400, mlx, window, color += color_change);
+	draw_line(450, 600, 400, 400, mlx, window, color += color_change);
+
+	// VERTICAL LINE DOWN --> UP HERE
+	draw_line(400, 600, 400, 400, mlx, window, color += color_change);
+
+	draw_line(350, 600, 400, 400, mlx, window, color += color_change);
+	draw_line(300, 600, 400, 400, mlx, window, color += color_change);
+	draw_line(250, 600, 400, 400, mlx, window, color += color_change);
+
+	draw_line(200, 600, 400, 400, mlx, window, color += color_change);
+
+	draw_line(200, 550, 400, 400, mlx, window, color += color_change);
+	draw_line(200, 500, 400, 400, mlx, window, color += color_change);
+	draw_line(200, 450, 400, 400, mlx, window, color += color_change);
+
+	draw_line(200, 400, 400, 400, mlx, window, color += color_change);
+
+	draw_line(200, 350, 400, 400, mlx, window, color += color_change);
+	draw_line(200, 300, 400, 400, mlx, window, color += color_change);
+	draw_line(200, 250, 400, 400, mlx, window, color += color_change);
+
+	draw_line(200, 200, 400, 400, mlx, window, color += color_change);
+
+	draw_line(250, 200, 400, 400, mlx, window, color += color_change);
+	draw_line(300, 200, 400, 400, mlx, window, color += color_change);
+	draw_line(350, 200, 400, 400, mlx, window, color += color_change);
+
+	mlx_key_hook(window, (void *)input_detective, mlx);
+	mlx_loop(mlx);
+}
+
 int			main (int ac, char **av)
 {
 	void	*mlx;
 	void	*window;
 	int		window_x;
 	int		window_y;
-	int		color;
-	int 	color_change = 8;
+	
 
 	if (ac == 2)
 	{
 		window_x = 800;
 		window_y = 800;
-		color = 140 * 65536; // RGB = (R*65536)+(G*256)+B , (when R is RED, G is GREEN and B is BLUE)
 
 		file_detective(av[1]);
-
 		mlx = mlx_init();
 		window = mlx_new_window(mlx, window_x, window_y, "detective");
-		// draw_line(400, 400, 200, 200, mlx, window, color);
-		// draw_line(400, 400, 600, 200, mlx, window, color);
-		// draw_line(400, 400, 600, 600, mlx, window, color);
-		// draw_line(400, 400, 600, 800, mlx, window, color);
-		// draw_line(400, 400, 600, 0, mlx, window, color);
-
-		// draw_line(200, 200, 400, 400, mlx, window, color);
-		// draw_line(600, 200, 400, 400, mlx, window, color);
-		// draw_line(600, 600, 400, 400, mlx, window, color);
-		// draw_line(200, 600, 400, 400, mlx, window, color);
-
-		// VERTICAL LINE UP --> DOWN HERE
-		draw_line(400, 200, 400, 400, mlx, window, color += color_change);
-
-		draw_line(450, 200, 400, 400, mlx, window, color += color_change);
-		draw_line(500, 200, 400, 400, mlx, window, color += color_change);
-		draw_line(550, 200, 400, 400, mlx, window, color += color_change);
-		
-		draw_line(600, 200, 400, 400, mlx, window, color += color_change);
-		
-		draw_line(600, 250, 400, 400, mlx, window, color += color_change);
-		draw_line(600, 300, 400, 400, mlx, window, color += color_change);
-		draw_line(600, 350, 400, 400, mlx, window, color += color_change);
-		
-		draw_line(600, 400, 400, 400, mlx, window, color += color_change);
-		
-		draw_line(600, 450, 400, 400, mlx, window, color += color_change);
-		draw_line(600, 500, 400, 400, mlx, window, color += color_change);
-		draw_line(600, 550, 400, 400, mlx, window, color += color_change);
-		
-		draw_line(600, 600, 400, 400, mlx, window, color += color_change);
-
-		draw_line(550, 600, 400, 400, mlx, window, color += color_change);
-		draw_line(500, 600, 400, 400, mlx, window, color += color_change);
-		draw_line(450, 600, 400, 400, mlx, window, color += color_change);
-
-		// VERTICAL LINE DOWN --> UP HERE
-		draw_line(400, 600, 400, 400, mlx, window, color += color_change);
-
-		draw_line(350, 600, 400, 400, mlx, window, color += color_change);
-		draw_line(300, 600, 400, 400, mlx, window, color += color_change);
-		draw_line(250, 600, 400, 400, mlx, window, color += color_change);
-
-		draw_line(200, 600, 400, 400, mlx, window, color += color_change);
-
-		draw_line(200, 550, 400, 400, mlx, window, color += color_change);
-		draw_line(200, 500, 400, 400, mlx, window, color += color_change);
-		draw_line(200, 450, 400, 400, mlx, window, color += color_change);
-
-		draw_line(200, 400, 400, 400, mlx, window, color += color_change);
-
-		draw_line(200, 350, 400, 400, mlx, window, color += color_change);
-		draw_line(200, 300, 400, 400, mlx, window, color += color_change);
-		draw_line(200, 250, 400, 400, mlx, window, color += color_change);
-
-		draw_line(200, 200, 400, 400, mlx, window, color += color_change);
-
-		draw_line(250, 200, 400, 400, mlx, window, color += color_change);
-		draw_line(300, 200, 400, 400, mlx, window, color += color_change);
-		draw_line(350, 200, 400, 400, mlx, window, color += color_change);
-
-		mlx_key_hook(window, (void *)input_detective, mlx);
-		mlx_loop(mlx);
+		test_print_spiral(mlx, window);
 	}
 	else
-		printf("wahoo!!!\n");
+		printf("didn't pass a file!!!\n");
 	return (0);
 }
