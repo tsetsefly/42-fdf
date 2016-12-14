9c3c36751b29f2d1acd198f7188c0569d64ddaf4
tsetsefly <tsetsefly@gmail.com>
2016-12-13 17:16:40 -0800

    connects lines now

--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -362,6 +362,7 @@ t_super		scale_init_map(t_super super_struct)
 		{
 			super_struct.map[i][j].scaled_x = x;
 			super_struct.map[i][j].scaled_y = y;
+			super_struct.map[i][j].scaled_z = 0;
 			x += super_struct.step_unit;
 			j++;
 		}
@@ -392,8 +393,33 @@ t_super		scale_that_shit(t_super super_struct)
 	printf("step unit = %f, axis = %c, start_x = %f, start_y = %f\n", super_struct.step_unit, super_struct.long_axis, super_struct.start_x, super_struct.start_y);
 	super_struct = scale_init_map(super_struct);
 	// need to draw lines now
-	print_shit(super_struct);
+	// print_shit(super_struct);
 	return (super_struct);
+}
+
+// void		draw_line(double x1, double y1, double x2, double y2, void *mlx, void *window, int color)
+void		connect_lines(t_super super_struct)
+{
+	int		i;
+	int		j;
+	int		color = 140 * 65536;
+
+	i = 0;
+	while (i < super_struct.rows)
+	{
+		j = 0;
+		while (j < super_struct.cols)
+		{
+			if (j < super_struct.cols - 1)
+				draw_line(super_struct.map[i][j].scaled_x, super_struct.map[i][j].scaled_y, super_struct.map[i][j + 1].scaled_x, super_struct.map[i][j + 1].scaled_y, super_struct.mlx, super_struct.window, color);
+			if (i < super_struct.rows - 1)
+				draw_line(super_struct.map[i][j].scaled_x, super_struct.map[i][j].scaled_y, super_struct.map[i + 1][j].scaled_x, super_struct.map[i + 1][j].scaled_y, super_struct.mlx, super_struct.window, color);
+			j++;
+		}
+		i++;
+	}
+	mlx_key_hook(super_struct.window, (void *)input_detective, super_struct.mlx);
+	mlx_loop(super_struct.mlx);
 }
 
 t_super		init_superstruct(char *av1)
@@ -408,6 +434,7 @@ t_super		init_superstruct(char *av1)
 	super_struct.mlx = mlx_init();
 	super_struct.window = mlx_new_window(super_struct.mlx, super_struct.window_x, super_struct.window_y, "detective");
 	super_struct = scale_that_shit(super_struct);
+	connect_lines(super_struct);
 	return (super_struct);
 }
 
